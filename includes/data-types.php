b93f681144baf575a5c96bd185a7f704bac54859
Exochos <35015441+Exochos@users.noreply.github.com>
2021-01-25 14:15:01 -0800

    new template

---
+++ includes/data-types.php
@@ -0,0 +1,20 @@
+<?php
+// Data Types
+
+$temp1 = '60';
+$temp2 = '40';
+$total = $temp1 + $temp2;
+$echo $total;
+echo '<br>';
+
+$name = 'Olga';     // String
+echo $name;
+echo '<br>';
+
+$temp = 70;       // Also is a string 
+if($temp === 60) {   // This is an int
+    echo 'Life is god';
+}
+else {
+    echo 'It is not identical';
+}
