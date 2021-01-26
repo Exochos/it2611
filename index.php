5faa45e96770dea9061f4e5f0c0fbee7cf217421
Exochos <35015441+Exochos@users.noreply.github.com>
2021-01-25 16:05:31 -0800

    Update index.php

--- index.php
+++ index.php
@@ -83,8 +83,8 @@
     Jeremy Ward	&copy; <?php echo date('Y'); ?>
     <br>
 
-                <li><a href="http://validator.w3.org/check?uri=referer">HTML</a><li>
-                <li><a href="http://jigsaw.w3.org/css-validator/check?uri=referer">CSS</a></li>
+                <a href="http://validator.w3.org/check?uri=referer">HTML</a>
+                <a href="http://jigsaw.w3.org/css-validator/check?uri=referer">CSS</a>
 </footer>
 </div><!-- End wrapper for the the whole thing-->
 <script>
