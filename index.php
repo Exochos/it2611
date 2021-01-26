f50a59bace4bfae3e84e2762e6eadb0e46019096
Exochos <35015441+Exochos@users.noreply.github.com>
2021-01-25 16:02:15 -0800

    Update index.php

--- index.php
+++ index.php
@@ -80,9 +80,11 @@
         </aside>
 </div>
 <footer>
-    Jeremy Ward	&copy; 2021<br>
-                <a href="http://validator.w3.org/check/referer" rel="nofollow" title="Validate as HTML5">HTML5</a>
-                <a href="http://jigsaw.w3.org/css-validator/validator?uri=<?php echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]; ?>" rel="nofollow" title="Validate as CSS3">CSS3</a>
+    Jeremy Ward	&copy; <?php echo date('Y'); ?>
+    <br>
+
+                <li><a href="http://validator.w3.org/check?uri=referer">HTML</a><li>
+                <li><a href="http://jigsaw.w3.org/css-validator/check?uri=referer">CSS</a></li>
 </footer>
 </div><!-- End wrapper for the the whole thing-->
 <script>
