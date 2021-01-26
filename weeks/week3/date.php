51260a781c4cbd5a45acf7edbd15aa2b2b5bb25f
Exochos <35015441+Exochos@users.noreply.github.com>
2021-01-25 18:24:56 -0800

    Update date.php

--- weeks/week3/date.php
+++ weeks/week3/date.php
@@ -4,6 +4,20 @@
 echo date('Y');
 echo '<br>';
 echo date('H:i A');
+echo '<br>';
+date_default_timezone_set('America/Los_Angeles');
+echo '<br>';
 
+$todayDate = ('H:i A');
+if ($todayDate <= 11) {
+    echo 'Good Morning!';
+}
+elseif ($todayDate <= 15) {
+    echo 'Good Afternoon';
+}
+else {
+    echo 'Good Evening';
+}
 
+// End Date
 ?>
