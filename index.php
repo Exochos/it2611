6cce4d4409c3a32f8783e852dc957981f1caa4ae
Exochos <35015441+Exochos@users.noreply.github.com>
2021-01-31 11:30:19 -0800

    updated

--- index.php
+++ index.php
@@ -1,44 +1,7 @@
 <?php 
     $title = 'Portal Page';
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- -->
-    <title><?= $title ?></title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <!-- Viewport so we can change depending on device size -->
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <!-- Style sheet & JS lives here -->
-    <link rel="stylesheet" href="css/style.css"> 
-    <script src="js/project.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
-<body>
-<div class='wrapper'><!-- Wrapper for the the whole thing-->
-
-    <!-- Header -->
-    <header class='header'>
-        <h1 class='title'>IT Portal Page V2.3</h1>
-    </header>
-    <!-- Nav -->
-    <nav class='nav'>  
-        <a href="#" class="icon">
-            <i class="fa fa-bars"></i>
-        </a>
-        <div class="right">
-            <a href="index.php">Home</a>
-            <a href="website/switch.php">HW3 Switch</a>
-            <a href="website/troubleshoot.php">HW4 Troubleshoot</a>
-            <a href="website/calculator.php">HW5 Calculator</a>
-            <a href="website/email.php">HW6 Email</a>
-            <a href="website/gallery.php">HW7 Gallery</a>
-            <a href="website/database.php">HW8 Database</a>
-        </div>
-    </nav>
-    <!-- End Nav -->
-
+<?php include('includes/header.php'); ?>
     <!-- Sub Wrapper -->
     <div class="main">
 
