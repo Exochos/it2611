0f43c7a084a91874db00f1113a170ff24c7ba746
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-01 12:50:39 -0800

    Create var2.php

---
+++ weeks/week2/var2.php
@@ -0,0 +1,25 @@
+<?php 
+// Assignments Week 2
+$money = 100;
+$money /= 7;
+$moneyFriendly = floor($money);
+$moneyFriendly = ceil($money);
+echo '<br>';
+echo $money;
+echo '<br>';
+echo $moneyFriendly;
+
+// Logic stuff
+echo '<br>';
+$radius = 10;
+$pie = 3.14;
+$circumference = (2 * $pie) * $radius;
+echo $circumference;
+
+// Celcious
+echo '<br>';
+$celcius = 22;
+$far = ($celcius * 9/5) + 32;
+echo $far;
+echo '<br>';
+?>
