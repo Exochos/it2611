d55b1266b04135ee7c1c9dac6d3bba2a77078690
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-01 16:45:21 -0800

    Update heredoc.php

--- weeks/week2/heredoc.php
+++ weeks/week2/heredoc.php
@@ -6,13 +6,13 @@ $author = 'Margaret Atwood';
 $character = 'June';
 $actor = 'Elizabeth Moss';
 $content = <<<EOT
-My Favorite book is $book, written by $author, and is presently a
+<p>My Favorite book is $book, written by $author, and is presently a
 miniseries on HULU. Hulu's viewing audience is extremely excited about the miniseries
-and looks forward to the 5th season of the award winning "$book!"
+and looks forward to the 5th season of the <b>award winning "$book!" </b></p>
 
-$actor's rendition as $character is right on! Once this content is corrected, it 
+<p><i>$actor's</i> rendition as $character is right on! Once this content is corrected, it 
 will display properly; my content is displayed in orange, and my variables are displayed 
-in black!!!
+in black!!!</p>
 EOT;
 
 echo $content;
