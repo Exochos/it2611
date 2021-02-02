8962ee2b956ba58be3df17e370c2ecbcbc7c8d08
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-01 17:09:18 -0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    Jeremy Ward	&copy; <?php echo date('Y'); ?><a href="http://validator.w3.org/check?uri=referer">HTML</a><a href="http://jigsaw.w3.org/css-validator/check?uri=referer">CSS</a>
+    Jeremy Ward	&copy; <?php echo date('Y'); ?><a href="http://validator.w3.org/check?uri=referer"> HTML </a><a href="http://jigsaw.w3.org/css-validator/check?uri=referer"> CSS </a>
 </footer>
 </div><!-- End wrapper for the the whole thing-->
 <script>
