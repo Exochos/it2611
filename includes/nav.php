5f1d6c8b25b3e2d62a93c3e44f1f3941be30b280
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-02 21:26:29 -0800

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -3,15 +3,15 @@
             <li>Weekly class Exercises</li>
                 <li>Week 2</li>
                 <li><ul class="second">
-                    <li><a href="weeks/week2/var2.php">var.php</a></li>
-                    <li><a href="weeks/week2/heredoc.php">heredoc.php</a></li>
-                    <li><a href="weeks/week2/currency-logic.php">currency-logic.php</a></li>
+                    <li><a href="http://jeremyward.me/it2611/weeks/week2/var2.php">var.php</a></li>
+                    <li><a href="http://jeremyward.me/it2611/weeks/week2/heredoc.php">heredoc.php</a></li>
+                    <li><a href="http://jeremyward.me/it2611/weeks/week2/currency-logic.php">currency-logic.php</a></li>
                 </ul></li>
                 <li>Week 3
                 <ul class="second">
-                    <li><a href="weeks/week3/date.php">date.php</a></li>
-                    <li><a href="weeks/week3/data-types.php">data-types.php</a></li>
-                    <li><a href="weeks/week3/switch.txt">switch.php</a></li>
+                    <li><a href="http://jeremyward.me/it2611/weeks/week3/date.php">date.php</a></li>
+                    <li><a href="http://jeremyward.me/it2611/weeks/week3/data-types.php">data-types.php</a></li>
+                    <li><a href="http://jeremyward.me/it2611/weeks/week3/switch.txt">switch.php</a></li>
                     <li><a href="#"></a><br></li>
                     <li></li>
                     <li>Week 4
