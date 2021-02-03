1a67395fccd70b149001a76219282c1217e23d90
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-02 20:39:34 -0800

    Update troubleshoot.php

--- website/troubleshoot.php
+++ website/troubleshoot.php
@@ -11,17 +11,17 @@
         <label>Enter the second number:</label><input type="text" name="num2"><br>
         <input type="submit" value="Add Em!!"> 
         </form>
+        <?php
+            if(isset($_POST['num1'], $_POST['num2'])) {
+                $num1 = $_POST['num1'];
+	            $num2 = $_POST['num2'];
+	            $myTotal = $num1 + $Num2; 
+                echo "<h2>You added " . $num1 . " and " . $num2 . "";
+            }
+        ?>
 </div>
 </main>
 <?php include('../includes/footer.php');?>
 </body>
 </html> 
 
-<?php
-if(isset($_POST['num1'], $_POST['num2'])) {
-        $num1 = $_POST['num1'];
-	    $num2 = $_POST['num2'];
-	    $myTotal = $num1 + $Num2; 
-        echo "<h2>You added " . $num1 . " and " . $num2 . "";
-    }
-?>
