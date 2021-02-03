a2326a7e0d805ae1a0b9bd6fb1da9ea59d0b855f
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-02 20:45:48 -0800

    Update troubleshoot.php

--- website/troubleshoot.php
+++ website/troubleshoot.php
@@ -11,19 +11,33 @@
 <div class="main">
 <?php include('../includes/nav.php'); ?>
 <main class='centerA'>
-    <form><h1>Adder.php</h1> </form action="post">
-        <label>Enter the first number:</label><input type="text" name="num1"><br>
-        <label>Enter the second number:</label><input type="text" name="num2"><br>
-        <input type="submit" value="Add Em!!"> 
-        </form>
-        <?php
-            if(isset($_POST['num1'], $_POST['num2'])) {
-                $num1 = $_POST['num1'];
-	            $num2 = $_POST['num2'];
-	            $myTotal = $num1 + $num2; 
-                echo "<h2>You added " . $num1 . " and " . $num2 . "";
-            }
-        ?>
+
+<form action method='post'>
+    <h1>Adder.php</h1>
+    <label>Enter the first number:</label><input type="text" name="num1"><br>
+    <label>Enter the second number:</label><input type="text" name="num2"><br>
+    <input type="submit" value="Add Em!!"> 
+</form>
+<?php
+if(isset($_POST['num1'], $_POST['num2'])) {
+
+	$num1 = $_POST['num1'];
+	$num2 = $_POST['num2'];
+	$myTotal += $num1 + $num2; 
+    echo "<h2>You added " . $num1 . " and " . $num2;
+    echo "<p> and the answer is <br>";
+    echo '<b>' . $myTotal .'</b>!</p>';
+    echo'<p><a href="">Reset page</a>';
+}
+else {
+    echo $num1;
+    echo $num2;
+}
+  #  
+   
+  # 
+
+?>
 </div>
 </main>
 <?php include('../includes/footer.php');?>
