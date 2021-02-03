d61974bf40c71651bb04afaac40f7ca88a5a9ced
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-02 21:28:08 -0800

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -6,20 +6,17 @@
                     <li><a href="http://jeremyward.me/it2611/weeks/week2/var2.php">var.php</a></li>
                     <li><a href="http://jeremyward.me/it2611/weeks/week2/heredoc.php">heredoc.php</a></li>
                     <li><a href="http://jeremyward.me/it2611/weeks/week2/currency-logic.php">currency-logic.php</a></li>
-                </ul></li>
                 <li>Week 3
                 <ul class="second">
                     <li><a href="http://jeremyward.me/it2611/weeks/week3/date.php">date.php</a></li>
                     <li><a href="http://jeremyward.me/it2611/weeks/week3/data-types.php">data-types.php</a></li>
                     <li><a href="http://jeremyward.me/it2611/weeks/week3/switch.txt">switch.php</a></li>
                     <li><a href="#"></a><br></li>
-                    <li></li>
                     <li>Week 4
                 <ul class="second">
                     <li><a href="http://jeremyward.me/it2611/weeks/week4/celcius.php">celcius</a></li>
                     <li><a href="http://jeremyward.me/it2611/weeks/week4/form1.php">form1</a></li>
                     <li><a href="http://jeremyward.me/it2611/weeks/week4/form2.php">form2</a></li>
-                    <li></li>
                 </ul>
         </menu>
         <!-- End Menu Tab Left -->
