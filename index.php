9340b26326750cbcf3b25b01e37b3a15d8561e93
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-02 22:14:30 -0800

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
            <h3>MAMP homework</h3><br>
            <a href="includes/info.php">Info.php page here to verify MAMP deployment</a><br>
            <a href="https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-on-ubuntu-20-04">Information I used to install everything</a><br>
-           <img src="images/jeremyerror.png"  width='60%' alt="error1">
-           <img src="images/jeremydisplayerrors.png" alt="error2">
+           <img src="images/jeremyerror.png"  width='80%' alt="error1">
+           <img src="images/jeremydisplayerrors.png" width='80%' alt="error2">
         </main>
         <?php include('includes/aside.php'); ?>
 </div>
