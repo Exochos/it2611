016aae4b9061b557ca6ac32648df1fad57367c62
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-05 14:45:53 -0800

    add

---
+++ weeks/week5/currency1a.php
@@ -0,0 +1,50 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+?>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Currency</title>
+  <meta name="description" content="Currency Form for IT162">
+  <meta name="author" content="Jeremy Ward">
+  <style>
+      form {
+          width: 300px;
+          border: 1px solid;
+      }
+  </style>
+</head>
+
+<form action="" method="post">
+    <fieldset>
+     <label for="name1">Name</label>
+     <input type="text" name="name" id="name1">
+     <label for="email1">Email</label>
+     <input type="email" name="email" id="email1">
+     <label for=>How much Money do you have?</label>
+     <input type="text" name="cash">
+     <input type="radio" id="age1" name="age" value="30">
+     <label for="age1">0 - 30</label><br>
+     <input type="radio" id="age2" name="age" value="60">
+     <label for="age2">31 - 60</label><br>  
+     <input type="radio" id="age3" name="age" value="100">
+     <label for="age3">61 - 100</label><br><br>
+     <input type="submit" value="Convert it!">
+     <p><a href="">Reset me</a></p>
+    </fieldset>
+<body>
+  
+</body>
+</html>
---
+++ weeks/week5/currency2.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+?>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Currency</title>
+  <meta name="description" content="Currency Form for IT162">
+  <meta name="author" content="Jeremy Ward">
+  <style>
+      
+  </style>
+</head>
+
+<body>
+  
+</body>
+</html>
