cf8556d8a98f48498dd3a582c9c1ccee92d8e5b7
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-09 11:00:26 -0800

    Update calculator.php

---
+++ website/calculator.php
@@ -0,0 +1,90 @@
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Gas Calculator</title>
+  <meta name="description" content="Gas Calculator Form for IT162">
+  <meta name="author" content="Jeremy Ward">
+  <style>
+    h1 {
+        text-align:center;
+    }
+    .box {
+        background-color: beige;
+        border: 1px solid;
+        padding-left: 10px;
+        padding-bottom:5px;
+      }
+      form {
+          width: 300px;
+          border: 1px solid;
+          background-color: beige;
+      }
+      label, input {
+          padding: 5px;
+          margin: 5px;
+      }
+  </style>
+</head>
+<body>
+<!-- Form below -->
+<div class="">
+<h1> Our Calculator </h1>
+<form action="" method="post">
+    <fieldset>
+     <label for="miles">How many miles will you be driving</label>
+     <input type="text" name="miles"><br>
+    
+      <ul><p><b>Price per gallon:</b></p><br>
+        <li><input type="radio" id="currency" name="currency" value="3.00">$3.00</li>
+        <li><input type="radio" id="currency" name="currency" value="3.50">$3.50</li> 
+        <li><input type="radio" id="currency" name="currency" value="4.00">$4.00</li>
+      </ul>
+
+     <input type="submit" value="Convert it!"><input type="submit" value="Reset">
+
+    </fieldset>
+</div>
+<?php
+/*
+    If fields are full
+    then calculate 
+    Otherwise throw an error relevent to the error.
+*/
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if (empty($_POST['name1'])) {
+        echo "Please enter your name<br>";
+    }
+    if(empty($_POST['email1'])) {
+        echo "Please enter your email<br>";
+    }
+    if(empty($_POST['cash1'])) {
+        echo "Please enter cash amount<br>";
+    }
+    if(empty($_POST['currency'])) {
+        echo "Please select a currency<br>";
+    }
+
+    if (isset($_POST['name1'],$_POST['email1'],$_POST['cash1'],$_POST['currency']) && is_numeric($_POST['cash1']) && is_numeric($_POST['currency']))  {
+        $name = $_POST['name1'];
+        $amount = $_POST['cash1'];
+        $currency = $_POST['currency'];
+        $total = $amount * $currency;
+
+
+        echo '<div class="box">';
+        echo '<h2>Hello '. $name .'</h2>'; 
+        echo "Your new total is: $" .$total . "";
+        echo '</div>';
+    }
+
+}
+
+
+?>
+</body>
+</html>
