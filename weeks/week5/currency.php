e4267a4578ec9818534af24849ff38cb3636969b
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-08 18:59:20 -0800

    Update currency.php

--- weeks/week5/currency.php
+++ weeks/week5/currency.php
@@ -64,6 +64,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         echo "Please select a currency<br>";
     }
 
+    if (isset($_POST['name1'],$_POST['email1'],$_POST['cash1'],$_POST['currency']) && is_numeric($_POST['cash1']) && is_numeric($_POST['currency']))  {
+                    echo "got here<br>";
+                }
+
 }
 
 
