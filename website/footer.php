cc25ad37c71de396194ddb88df1b3d11af013c26
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-20 13:30:55 -0800

    a

--- website/footer.php
+++ website/footer.php
@@ -1,3 +1,3 @@
 <footer>
-    Jeremy Ward	&copy; <?php echo date('Y'); ?> All rights reserves<a href="http://validator.w3.org/check?uri=referer"> HTML </a><a href="http://jigsaw.w3.org/css-validator/check?uri=referer"> CSS </a><a href="../index.php">Portal Page</a>
+    Jeremy Ward	&copy; 2020 - <?php echo date('Y'); ?> All rights reserves<a href="http://validator.w3.org/check?uri=referer"> HTML </a><a href="http://jigsaw.w3.org/css-validator/check?uri=referer"> CSS </a><a href="../index.php">Portal Page</a>
 </footer>
