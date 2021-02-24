9d8c0e279e0406c449624b3eb18e4b8d878fc0e3
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-23 20:02:02 -0800

    Create thx.php

---
+++ website/thx.php
@@ -0,0 +1,9 @@
+<!-- includer config and header -->
+<?php include('includes/config.php'); ?>
+<?php include('includes/header.php'); ?>
+
+<div> 
+   <b> Thanks for submitting to our form! </b>
+</div>
+<!-- Include -- Footer -->        
+<?php include('includes/footer.php'); ?>
