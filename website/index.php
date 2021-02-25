abf9abddd229742bb32c37a5ec8078ae37dda325
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-24 18:30:15 -0800

    asdasdasd

--- website/index.php
+++ website/index.php
@@ -2,6 +2,23 @@
 <?php include('includes/config.php'); ?>
 <?php include('includes/header.php'); ?>
 
+<!-- Begin body content -->
+<div class="container" style="margin-top:30px">
+    <!-- row here -->
+    <div class="row">
+        <div class="col-sm-8">
 
+            <?php
+                echo '<img class="card" src="images/'. rand(0,6) . '.jpg">';
+            ?>
+
+        </div>
+        <div class="col-sm-4">
+            <p>
+            Random cute doggo here! Refresh to see a new one
+            </p>
+        </div>
+    </div>
+</div>
 <!-- Include -- Footer -->        
 <?php include('includes/footer.php'); ?>
