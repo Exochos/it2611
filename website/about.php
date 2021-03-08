e21564b6e5f1faddf6cc78feb84f597f00126cb9
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-08 13:40:19 -0800

    aaa

--- website/about.php
+++ website/about.php
@@ -1,6 +1,16 @@
 <?php include('includes/config.php'); ?>
 <?php include('includes/header.php'); ?>
-
+<!-- Begin body content -->
+<div class="container" style="margin-top:30px">
+    <!-- row here -->
+    <div class="row">
+        <div class="col-sm-8">
+            <img src="table.png" alt="SQLtable">
+        </div>
+        <div class="col-sm-4">
+        </div>
+    </div>
+</div>
 
 <!-- Include -- Footer -->        
 <?php include('includes/footer.php'); ?>
