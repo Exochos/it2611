d9b71f210b5ccd43c768346e4038e225f56060f6
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-08 12:41:09 -0800

    Update view.php

--- website/view.php
+++ website/view.php
@@ -27,9 +27,9 @@ try {
         <li><h1>Dog Name: <?php echo htmlspecialchars($row['dogName']); ?></h1></li>
         <li><h3>Dog Breed: <?php echo htmlspecialchars($row['dogBreed']) ?></h3></li>
         <li><h3>Dog Age: <?php echo htmlspecialchars($row['dogAge']); ?></h3></li>
-        <li><?php echo htmlspecialchars($row['dogWeight']); ?></li>
-        <li><?php echo htmlspecialchars($row['dogDesc']); ?></li>
-        <li><img src="images/<?php echo htmlspecialchars($row['dogImg']); ?>.jpg" alt=""></li>
+        <li><h3>Dog Weight: <?php echo htmlspecialchars($row['dogWeight']); ?></h3></li>
+        <li><h4><?php echo htmlspecialchars($row['dogDesc']); ?></h4></li>
+        <li><img src="images/<?php echo htmlspecialchars($row['dogImg']); ?>.jpg" alt="dog image"></li>
     </UL>
 <?php endwhile; ?>
 </table>
