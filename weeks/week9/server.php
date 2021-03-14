a3fc980ff5bab7852c73508d7e30869115813b24
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-14 14:55:36 -0700

    Update server.php

--- weeks/week9/server.php
+++ weeks/week9/server.php
@@ -4,7 +4,7 @@
     // md5()  
 
     //session_start();
-    require_once '../../../dbconfig.php';
+    require_once 'config.php';
 
     //initialize
 
@@ -16,7 +16,7 @@
     $succcess = 'You are not logged in!';
 
     // Connect to the database
-
+    echo "got here";
     $db = mysqli_connect($DB_HOST, $DB_USER,$DB_PASSWORD,$DB_NAME) or die(myError(__FILE__,__LINE__,mysqli_connect_error()));
     echo $db;
 
