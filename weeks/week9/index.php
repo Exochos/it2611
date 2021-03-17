0768f60b4e70e240d3c738be669d75968b35cebc
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-17 16:21:55 -0700

    Update index.php

--- weeks/week9/index.php
+++ weeks/week9/index.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     //include('');
+    print_r ($_SESSION);
     echo $_SESSION['success'] . '<br>';
     echo "Thanks!" . $_SESSION['username'] . " Thanks for joining us!";
 ?>
