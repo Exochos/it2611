eb210ecc757fa93ff209b2d7b0816ca084d59803
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-23 19:26:59 -0700

    Update contact.php

--- finalProject/contact.php
+++ finalProject/contact.php
@@ -7,8 +7,8 @@
 
     <!-- Begin page content -->
     <main role="main" class="container">
-      <h1 class="mt-5">Contact Us!</h1>
       <p>
+        <h1 class="mt-5">Contact Us!</h1>
         <div class="row">
             <div class="col-sm-8">
                 <div class="form group">
