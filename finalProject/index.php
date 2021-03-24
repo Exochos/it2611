8de76b3fb1c27772a5893de99a82cdda13dc87ca
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-23 17:34:41 -0700

    Update index.php

--- finalProject/index.php
+++ finalProject/index.php
@@ -24,22 +24,12 @@
         //todo
         echo '<main role="main" class="container"><!-- Container -->
         <div class="row"><!-- Row -->
-          <div class="col-sm-8">
-            <h2><b><?php echo $alt ?></b></h2> 
-            <img src="<?php echo "images/" . $pic; ?>.jpg" alt="" style="width:100%">    
-                <p><?php echo $p ?></p><br>
-          </div>
-            
+          <div class="col-sm-8">';
+            echo ' Welcome ' . $_SESSION['username'] . '</div>';
+          
+        echo '
         <div class="col-sm-4">
             <aside>
-              <b>Want more Houses?</b></br>
-              <a href="switch.php?today=Monday">Monday</a><br>
-              <a href="switch.php?today=Tuesday">Tuesday<a><br>
-              <a href="switch.php?today=Wednesday">Wednesday<a><br>
-              <a href="switch.php?today=Thursday">Thursday<a><br>
-              <a href="switch.php?today=Friday">Friday<a><br>
-              <a href="switch.php?today=Saturday">Saturday<a><br>
-              <a href="switch.php?today=Sunday">Sunday<a><br>
           </aside>
         </div>
         </div><!-- Row -->
