6d85437e5eaa181fca52420514c24ed0e1fcb553
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-23 17:48:45 -0700

    Update config.php

--- finalProject/includes/config.php
+++ finalProject/includes/config.php
@@ -26,8 +26,12 @@ switch ($day) {
         break;
 case "Wednesday":
         $pic = 'h3';
-        $alt = '';
-        $p = '';
+        $alt = 'This ultra-modern home is in Hunts Point, a small affluent town on Lake Washington.';
+        $p = 'A $45 million ultra-modern mansion is the most expensive home in the Seattle area.<br>
+        The home sits on the shores of Lake Washington in affluent Hunts Point, about a 20-minute drive from downtown Seattle.<br>
+        The 9,420-square-foot, three-bedroom mansion includes 300 feet of prime waterfront, a private boat dock, and a rooftop deck complete with a fireplace and native plant garden.<br>
+        <a href="https://www.businessinsider.com/most-expensive-home-seattle-area-mansion-hunts-point-photos-2019-3">Find out more</a>
+        ';
         break;
 case "Thursday":
         $pic = 'h4';
