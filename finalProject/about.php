b76c0186e986265cf0ec1a4193a77f9240c30f5e
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-24 16:04:06 -0700

    Update about.php

--- finalProject/about.php
+++ finalProject/about.php
@@ -14,13 +14,13 @@ include('includes/header.php');
           <p> Seattles best houses was founded in 1993 when the late Hiro Protaganist founded the site.</p>
           <h4> Mission Statement </h4>
           <p>Seattles Best Home's mission is to modernize and progress the experience of buying and selling real estate by cultivating a spirit of collaboration, innovation, and integrity.<br>
-We strive to foster a culture of partnership in which all clients and listings are represented in a cooperative environment by all its agents, thereby ensuring its clients and listings have the competitive edge.” <b>Hiro Protaganist, CEO and co-founder.</b><BR>
-“Leveraging the most emergent technologies and social media strategies, The Agency envisions itself as more than just a real estate brokerage; it is a lifestyle company committed to informing and connecting global communities.” Billy Rose, President and co-founder.</p>
+            We strive to foster a culture of partnership in which all clients and listings are represented in a cooperative environment by all its agents, thereby ensuring its clients and listings have the competitive edge.” <b>Hiro Protaganist, CEO and co-founder.</b><BR>
+“Leveraging the most emergent technologies and social media strategies, We envision ourselves as more than just a real estate brokerage; it is a lifestyle company committed to informing and connecting global communities.” Billy Rose, President and co-founder.</p>
           </div> <!-- End COL SM 8 -->
         <div class="col-sm-4">
             <aside>
             <h2> About US </h2>
-            <h4>Tucker Ferdinand, Fredruck Mustifar, Dustin Moskovitz, Leon Black & Stanley Kroenke</h4>
+            <h4>Hiro Protaganist, CEO and co-founder. Billy Rose CFO and co-founder, Tucker Ferdinand, Fredruck Mustifar, Dustin Moskovitz, Leon Black & Stanley Kroenke</h4>
             <img src="https://www.businesstimes.com.sg/sites/default/files/styles/article_img/public/image/2019/10/25/BT20191025_ENTERTAINMENT.jpg?itok=t8tAipdi" alt="Rich White People">
           </aside>
         </div>
