6035c9a2c111fd00113229ba0f03787638e5e02c
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-23 22:25:27 -0700

    Update about.php

--- finalProject/about.php
+++ finalProject/about.php
@@ -10,13 +10,12 @@ include('includes/header.php');
 <main class="container"><!-- Container -->
         <div class="row"><!-- Row -->
           <div class="col-sm-8">
-          <h2> About US
+          <h2> About US </h2>
           </div> <!-- End COL SM 8 -->
         <div class="col-sm-4">
             <aside>
-            <p> Tucker Ferdinand, Fredruck Mustifar, Blahs
+            <h3>Tucker Ferdinand, Fredruck Mustifar, Dustin Moskovitz, Leon Black, Stanley Kroenke</h3>
             <img src="https://www.businesstimes.com.sg/sites/default/files/styles/article_img/public/image/2019/10/25/BT20191025_ENTERTAINMENT.jpg?itok=t8tAipdi" alt="Rich White People">
-            </p>
           </aside>
         </div>
         </div><!-- Row -->
