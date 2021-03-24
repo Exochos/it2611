05a03c31c266a3614419832152befa0c7f92ea58
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-23 17:32:01 -0700

    Update index.php

--- finalProject/index.php
+++ finalProject/index.php
@@ -22,8 +22,28 @@
     }
     else {
         //todo
-        echo 'got here';
-        echo $_SESSION['login'];
+        echo '<main role="main" class="container"><!-- Container -->
+        <div class="row"><!-- Row -->
+          <div class="col-sm-8">
+            <h2><b><?php echo $alt ?></b></h2> 
+            <img src="<?php echo "images/" . $pic; ?>.jpg" alt="" style="width:100%">    
+                <p><?php echo $p ?></p><br>
+          </div>
+            
+        <div class="col-sm-4">
+            <aside>
+              <b>Want more Houses?</b></br>
+              <a href="switch.php?today=Monday">Monday</a><br>
+              <a href="switch.php?today=Tuesday">Tuesday<a><br>
+              <a href="switch.php?today=Wednesday">Wednesday<a><br>
+              <a href="switch.php?today=Thursday">Thursday<a><br>
+              <a href="switch.php?today=Friday">Friday<a><br>
+              <a href="switch.php?today=Saturday">Saturday<a><br>
+              <a href="switch.php?today=Sunday">Sunday<a><br>
+          </aside>
+        </div>
+        </div><!-- Row -->
+      </main><!-- Container -->';
     }
     include('includes/footer.php');
 
