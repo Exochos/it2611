2b8c7de2f1567b7163349212ab009417f15c83e9
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-24 16:49:04 -0700

    Update data.php

--- finalProject/data.php
+++ finalProject/data.php
@@ -3,8 +3,12 @@
 
     <!-- Begin page content -->
     <main role="main" class="container">
-      <h1 class="mt-5">             </h1>
-      <p><iframe src="https://www.google.com/maps/d/u/0/embed?mid=1h5_4gqWHkpTqzYYgb317Y7BuOV5Oid-m" width="640" height="480"></iframe></p>
+    <div class="row">
+            <div class="col-sm-8">
+              <h1 class="mt-5">Houses in our Database:</h1>
+            </div>
+    <div class="col-sm-4">
+    <p><iframe src="https://www.google.com/maps/d/u/0/embed?mid=1h5_4gqWHkpTqzYYgb317Y7BuOV5Oid-m" width="640" height="480"></iframe></p>
+    </div>
     </main>
-
 <?php include('includes/footer.php'); ?>
