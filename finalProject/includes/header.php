081ce3f632ca0d7d08c7d82237b46c55c2f8551d
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-23 22:20:48 -0700

    Update header.php

--- finalProject/includes/header.php
+++ finalProject/includes/header.php
@@ -16,9 +16,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet"  type='text/css'>
     <link href="css/css.css" rel="stylesheet">
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-    <meta http-equiv="Pragma" content="no-cache" />
-    <meta http-equiv="Expires" content="0" />
   </head>
 
   <body>
@@ -26,7 +23,7 @@
     <header>
       <!-- Fixed navbar -->
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#"><img src="images/home.png" alt="home icon" height="50px"><b> Seattles Best Houses </b></a>
+        <a class="navbar-brand" href="#"><img src="images/home.png" alt="home icon" height="50"><b> Seattles Best Houses </b></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
