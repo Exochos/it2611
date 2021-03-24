5d7bd67e97a7ae95eab46ceaaff18ef586688aa1
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-23 22:27:20 -0700

    Update about.php

--- finalProject/about.php
+++ finalProject/about.php
@@ -10,11 +10,13 @@ include('includes/header.php');
 <main class="container"><!-- Container -->
         <div class="row"><!-- Row -->
           <div class="col-sm-8">
-          <h2> About US </h2>
+          <h3>Seattles Best Houses</h3>
+          <p> Seattles best houses was founded in 1993 when the late Hiro Protaganist founded the site.</p>
           </div> <!-- End COL SM 8 -->
         <div class="col-sm-4">
             <aside>
-            <h3>Tucker Ferdinand, Fredruck Mustifar, Dustin Moskovitz, Leon Black, Stanley Kroenke</h3>
+            <h2> About US </h2>
+            <h4>Tucker Ferdinand, Fredruck Mustifar, Dustin Moskovitz, Leon Black & Stanley Kroenke</h4>
             <img src="https://www.businesstimes.com.sg/sites/default/files/styles/article_img/public/image/2019/10/25/BT20191025_ENTERTAINMENT.jpg?itok=t8tAipdi" alt="Rich White People">
           </aside>
         </div>
