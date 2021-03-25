043ece41479859faa2a35825bc8a7b2cd1b37a09
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-25 12:09:37 -0700

    Update data.php

--- finalProject/data.php
+++ finalProject/data.php
@@ -21,8 +21,9 @@
             <div class="col-sm-8">
               <h1 class="mt-5">Houses in our Database:</h1>
               <?php
-                print_r($rows);
                 echo $rows['homeid'];
+                echo $rows[0];
+                echo $rows[1];
               ?>
             <table class="table table-bordered table-condensed">
                 <thead>
