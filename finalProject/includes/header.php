3576081baa1d7a4d091eb9985963d3ccc588b399
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-26 13:50:02 -0700

    Update header.php

--- finalProject/includes/header.php
+++ finalProject/includes/header.php
@@ -33,21 +33,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="about.php">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="data.php">Homes</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="switch.php">Daily Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contact.php">Contact</a>
-            </li>
+            <?php makeLinks($nav); ?>
           </ul>
         </div>
       </nav>
