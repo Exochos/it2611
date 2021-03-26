3451d28f927052e9654754b759d5147a29cbf239
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-25 17:24:44 -0700

    Update view.php

--- finalProject/view.php
+++ finalProject/view.php
@@ -20,7 +20,6 @@ $results = mysqli_query($db,$query);
                     </td>
                     </tr>
                     <tr>
-                        <td><a href="view.php?id=<?php echo htmlspecialchars($row['homeid']); ?> ?>"><?php echo htmlspecialchars($row['homeid']); ?></a></td>
                         <td><?php echo htmlspecialchars($row['hometitle']) ?></td>
                         <td>$<?php echo htmlspecialchars($row['homeprice']); ?></td>
                     </tr>
@@ -33,13 +32,6 @@ $results = mysqli_query($db,$query);
             </table>
             </div>
     <div class="col-sm-4">
-    <p><iframe src="https://www.google.com/maps/d/u/0/embed?mid=1h5_4gqWHkpTqzYYgb317Y7BuOV5Oid-m" width="640" height="480"></iframe></p>
     </div>
     </main>
-<div class="container" style="margin:0 auto;">
-<table>
-
-
-</table>
-</container>
 <?php include('includes/footer.php'); ?>
